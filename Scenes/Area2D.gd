e3d20a766ccc33654b7f8f2e31768eb59002b11f
xPvtPancakes <50588259+xPvtPancakes@users.noreply.github.com>
2024-01-20 13:12:29 -0500

    more effects

--- Scenes/Area2D.gd
+++ Scenes/Area2D.gd
@@ -15,15 +15,17 @@ func _on_body_entered(body):
 		PlayerVariables.emit_signal("score_up", 20)
 		$Sheet.set_deferred("visible", false)
 		$CollisionShape2D.set_deferred("disabled", true)
-		$AnimatedSprite2D.play("default")
-		$AnimatedSprite2D.set_deferred("visible", true)
+		$Explosion_Sprite.play("default")
+		$Explosion_Sprite.set_deferred("visible", true)
 	if body.is_in_group("boss"):
 		$Explosion_SE.play()
 		speed = 0
 		$Sheet.set_deferred("visible", false)
 		$CollisionShape2D.set_deferred("disabled", true)
-		$AnimatedSprite2D.play("default")
-		$AnimatedSprite2D.set_deferred("visible", true)
+		$Boss_Explosion_Sprite.play("default")
+		$Boss_Explosion_Sprite.set_deferred("visible", true)
+		$Boss_Explosion_Sprite2.play("default")
+		$Boss_Explosion_Sprite2.set_deferred("visible", true)
 		
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
@@ -34,5 +36,13 @@ func _on_explosion_se_finished():
 	queue_free()
 
 
-func _on_animated_sprite_2d_animation_finished():
-	$AnimatedSprite2D.set_deferred("visible", false)
+
+func _on_explosion_sprite_animation_finished():
+	$Explosion_Sprite.set_deferred("visible", false)
+
+
+func _on_boss_explosion_sprite_animation_finished():
+	$Boss_Explosion_Sprite.set_deferred("visible", false)
+	$Boss_Explosion_Sprite2.set_deferred("visible", false)
+
+
